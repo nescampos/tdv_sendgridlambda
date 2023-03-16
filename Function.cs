9a357d706b8a0570348dd8908f8c67725d852639
Néstor Nicolás Campos Rojas <nescampos_18@hotmail.com>
2023-03-16 17:12:41 -0300

    Update Function.cs

--- Function.cs
+++ Function.cs
@@ -1,49 +1,45 @@
-using System.Text;
 using Amazon.Lambda.Core;
 using Amazon.Lambda.S3Events;
+using Amazon.Lambda.Serialization.SystemTextJson;
 using SendGrid;
 using SendGrid.Helpers.Mail;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
-[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+[assembly: LambdaSerializer(typeof(DefaultLambdaJsonSerializer))]
 
 namespace IncomingS3FilesMonitor;
 
 public class Function
 {
-    
     private string SendGridApiKey = Environment.GetEnvironmentVariable("SendGridApiKey");
     private string FromEmailAddress = Environment.GetEnvironmentVariable("FromEmailAddress");
     private string ToEmailAddress = Environment.GetEnvironmentVariable("ToEmailAddress");
 
-
     public async Task FunctionHandler(S3Event s3Event, ILambdaContext context)
     {
+        var client = new SendGridClient(SendGridApiKey);
         foreach (var s3EventRecord in s3Event.Records)
         {
             var s3Object = s3EventRecord.S3.Object;
             var objectKey = s3Object.Key;
 
-            //var objectMetadata = await GetObjectMetadataAsync(objectKey);
+            var message = MailHelper.CreateSingleEmail(
+                from: new EmailAddress(FromEmailAddress), 
+                to: new EmailAddress(ToEmailAddress), 
+                subject: "New file uploaded to S3", 
+                plainTextContent: $"New file uploaded to S3: {objectKey}\n\n" +
+                    $"Size of the file (in bytes): {s3Object.Size}", 
+                htmlContent: null
+            );
 
-            var emailContent = new StringBuilder();
-            emailContent.Append($"New file uploaded to S3: {objectKey}\n\n");
-            emailContent.Append($"Size of the file (in bytes): {s3Object.Size}\n\n");
-
-            await SendEmailAsync(emailContent.ToString());
+            var response = await client.SendEmailAsync(message);
+            if(!response.IsSuccessStatusCode)
+            {
+                var errorMessage = "Email failed to sent." +
+                    $"Response status code: {response.StatusCode}, " +
+                    $"body: {await response.Body.ReadAsStringAsync()}";
+                Console.Error.WriteLine(errorMessage);
+            }
         }
-    }
-
-
-    private async Task SendEmailAsync(string content)
-    {
-        var client = new SendGridClient(SendGridApiKey);
-        var from = new EmailAddress(FromEmailAddress);
-        var to = new EmailAddress(ToEmailAddress);
-        var subject = "New file uploaded to S3";
-        var plainTextContent = content;
-        var htmlContent = $"<pre>{content}</pre>";
-        var message = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
-        var response = await client.SendEmailAsync(message);
     }
 }
